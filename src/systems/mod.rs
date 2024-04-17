d08b046850c5c76bdddf23ec0bcd3d4e1dabdaf2
lucas-perata <lucas.perata@hotmail.com>
2024-04-16 21:31:08 -0300

    collisions

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -8,6 +8,7 @@ mod player_input;
 pub fn build_scheduled() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
