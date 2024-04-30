feb8957159d6caa9d51b1478313ae7d7dff721ed
lucas-perata <lucas.perata@hotmail.com>
2024-04-30 13:03:39 -0300

    fix automata maps

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -3,97 +3,6 @@ use crate::prelude::*;
 
 pub struct CellularAutomataArchitect {}
 
-impl CellularAutomataArchitect {
-    fn random_noise_map(&mut self, rng: &mut RandomNumberGenerator, map: &mut Map) {
-        map.tiles.iter_mut().for_each(|t| {
-            let roll = rng.range(0, 100);
-            if roll > 55 {
-                *t = TileType::Floor;
-            } else {
-                *t = TileType::Wall;
-            }
-        })
-    }
-
-    fn count_neighbors(&self, x: i32, y: i32, map: &Map) -> usize {
-        let mut neighbours = 0;
-        // TODO: out of bounds error
-        for iy in -1..=1 {
-            for ix in -1..=1 {
-                if !(ix == 0 && iy == 0) && map.tiles[map_idx(x + ix, y + iy)] == TileType::Wall {
-                    neighbours += 1;
-                }
-            }
-        }
-        neighbours
-    }
-
-    fn iteration(&mut self, map: &mut Map) {
-        // new map
-        let mut new_tiles = map.tiles.clone();
-        // iterate avoiding the empty map
-        for y in 1..=SCREEN_HEIGHT - 1 {
-            for x in 1..=SCREEN_WIDTH - 1 {
-                let neighbours = self.count_neighbors(x, y, map);
-                let idx = map_idx(x, y);
-                if neighbours > 4 || neighbours == 0 {
-                    new_tiles[idx] = TileType::Wall;
-                } else {
-                    new_tiles[idx] = TileType::Floor;
-                }
-            }
-        }
-        map.tiles = new_tiles;
-    }
-
-    // fn find_start3(&self, map: &Map) -> Point {
-    //     // map center
-    //     let center = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-    //
-    //     let closest_point = map
-    //         .tiles
-    //         .iter()
-    //         .enumerate()
-    //         // each iteration contains a tuple (index, tiletype)
-    //         .filter(|(_, t)| **t == TileType::Floor)
-    //         .map(|(idx, _)| {
-    //             (
-    //                 idx,
-    //                 DistanceAlg::Pythagoras.distance2d(center, map.index_to_point2d(idx)),
-    //             )
-    //         })
-    //         .min_by(|(_, distance), (_, distance2)| distance.partial_cmp(&distance2).unwrap())
-    //         .map(|(idx, _)| idx)
-    //         .unwrap();
-    //     map.index_to_point2d(closest_point);
-    // }
-
-    fn find_start(&self, map: &Map) -> Point {
-        let center = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2); // (10)
-        let closest_point = map
-            .tiles
-            .iter() // (11)
-            .enumerate() // (12)
-            .filter(|(_, t)| **t == TileType::Floor) // (13)
-            .map(|(idx, _)| {
-                (
-                    idx,
-                    DistanceAlg::Pythagoras.distance2d(
-                        // (14)
-                        center,
-                        map.index_to_point2d(idx),
-                    ),
-                )
-            })
-            .min_by(
-                |(_, distance), (_, distance2)| distance.partial_cmp(&distance2).unwrap(), // (15)
-            )
-            .map(|(idx, _)| idx) // (16)
-            .unwrap(); // (17)
-        map.index_to_point2d(closest_point) // (18)
-    }
-}
-
 impl MapArchitect for CellularAutomataArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
         let mut mb = MapBuilder {
@@ -113,5 +22,66 @@ impl MapArchitect for CellularAutomataArchitect {
         mb.player_start = start;
         mb.amulet_start = mb.find_most_distant();
         mb
+    }
+}
+
+impl CellularAutomataArchitect {
+    fn random_noise_map(&mut self, rng: &mut RandomNumberGenerator, map: &mut Map) {
+        map.tiles.iter_mut().for_each(|t| {
+            let roll = rng.range(0, 100);
+            if roll > 55 {
+                *t = TileType::Floor;
+            } else {
+                *t = TileType::Wall;
+            }
+        });
+    }
+
+    fn count_neighbors(&self, x: i32, y: i32, map: &Map) -> usize {
+        let mut neighbors = 0;
+        for iy in -1..=1 {
+            for ix in -1..=1 {
+                if !(ix == 0 && iy == 0) && map.tiles[map_idx(x + ix, y + iy)] == TileType::Wall {
+                    neighbors += 1;
+                }
+            }
+        }
+
+        neighbors
+    }
+
+    fn iteration(&mut self, map: &mut Map) {
+        let mut new_tiles = map.tiles.clone();
+        for y in 1..SCREEN_HEIGHT - 1 {
+            for x in 1..SCREEN_WIDTH - 1 {
+                let neighbors = self.count_neighbors(x, y, map);
+                let idx = map_idx(x, y);
+                if neighbors > 4 || neighbors == 0 {
+                    new_tiles[idx] = TileType::Wall;
+                } else {
+                    new_tiles[idx] = TileType::Floor;
+                }
+            }
+        }
+        map.tiles = new_tiles;
+    }
+
+    fn find_start(&self, map: &Map) -> Point {
+        let center = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+        let closest_point = map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(_, t)| **t == TileType::Floor)
+            .map(|(idx, _)| {
+                (
+                    idx,
+                    DistanceAlg::Pythagoras.distance2d(center, map.index_to_point2d(idx)),
+                )
+            })
+            .min_by(|(_, distance), (_, distance2)| distance.partial_cmp(distance2).unwrap())
+            .map(|(idx, _)| idx)
+            .unwrap();
+        map.index_to_point2d(closest_point)
     }
 }
